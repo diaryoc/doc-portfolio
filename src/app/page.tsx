31399dbba1072cd3a675466e8d63447e163c0f7d
diaryoc <diarycuriosity@gmail.com>
2024-06-06 18:09:04 +0700

    #1-make apply button token
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import Button from "@/components/tokens/button";
 
 export default function Home() {
-  return (
-    <main>hello word!</main>
+    return (
+    <main>
+      <div className={"p-6"}>
+        <Button typeName={"button"} className={"btn btn-primary mr-3"} name={"Primary"}/>
+        <Button typeName={"button"} className={"btn btn-success mr-3"} name={"Success"}/>
+        <Button typeName={"button"} className={"btn btn-info mr-3"} name={"Info"}/>
+        <Button typeName={"button"} className={"btn btn-danger mr-3"} name={"Danger"}/>
+        <Button typeName={"button"} className={"btn btn-warning mr-3"} name={"Warning"}/>
+      </div>
+    </main>
   );
 }
