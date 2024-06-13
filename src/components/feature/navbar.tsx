4338b77414556ef55b755789133cde08b08048d6
diaryoc <diarycuriosity@gmail.com>
2024-06-13 18:43:52 +0700

    #2-implement logo token
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/navbar.tsx
+++ src/components/feature/navbar.tsx
@@ -1,9 +1,10 @@
+import Logo from "@/components/tokens/logo";
 
 
 export default function Navbar(){
     return(
         <nav className="flex justify-between items-center h-20 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32">
-
+            <Logo/>
             <div className=""> hamburger</div>
         </nav>
     )
