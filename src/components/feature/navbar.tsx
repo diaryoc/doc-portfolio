b7062dde5b6ab5b637d76123972e880836ca11a5
diaryoc <diarycuriosity@gmail.com>
2024-06-13 18:42:57 +0700

    #2-make navbar component
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

---
+++ src/components/feature/navbar.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Navbar(){
+    return(
+        <nav className="flex justify-between items-center h-20 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32">
+
+            <div className=""> hamburger</div>
+        </nav>
+    )
+}
