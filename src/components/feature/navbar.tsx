26f7efe70f07bc943993651104d6b0c0b59db93b
diaryoc <diarycuriosity@gmail.com>
2024-06-18 16:35:27 +0700

    #2-adding navbar item and set up navbar component
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/navbar.tsx
+++ src/components/feature/navbar.tsx
@@ -1,11 +1,16 @@
 import Logo from "@/components/tokens/logo";
+import ListComponent from "@/components/component/listComponent";
+import {getNavbar} from "@/data/navbar";
 
 
 export default function Navbar(){
+    const dataNavbar = getNavbar();
     return(
         <nav className="flex justify-between items-center h-20 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32">
             <Logo/>
-            <div className=""> hamburger</div>
+            <div className="">
+                <ListComponent list={dataNavbar} ulClass={"flex justify-between gap-4"} />
+            </div>
         </nav>
     )
 }
---
+++ src/data/navbar.ts
@@ -0,0 +1,26 @@
+const navbar = [
+    {
+        id:1,
+        name:"Hero",
+        link:"/"
+    },
+    {
+        id:2,
+        name:"About",
+        link:"/"
+    },
+    {
+        id:3,
+        name:"Projects",
+        link:"/"
+    },
+    {
+        id:4,
+        name:"Contact Us",
+        link:"/"
+    },
+]
+
+export const getNavbar = ()=>{
+    return navbar;
+}
