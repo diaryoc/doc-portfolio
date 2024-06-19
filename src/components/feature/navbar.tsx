e56bcac197be89fea02b67b3302b5421e1b77d88
diaryoc <diarycuriosity@gmail.com>
2024-06-19 07:50:14 +0700

    #2-styling nav hover
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/navbar.tsx
+++ src/components/feature/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar(){
         <nav className="flex justify-between items-center h-20 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32">
             <Logo/>
             <div className="">
-                <ListComponent list={dataNavbar} ulClass={"flex justify-between gap-4"} />
+                <ListComponent list={dataNavbar} ulClass={"flex justify-between gap-4"} itemClass={"relative font-medium text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-primary after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"} />
             </div>
         </nav>
     )
