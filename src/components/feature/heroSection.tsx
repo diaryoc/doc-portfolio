e0c7c24e0f5aefb6f4c9aab56dd28b5672452b8c
diaryoc <diarycuriosity@gmail.com>
2024-06-28 07:43:40 +0700

    #3-test hero sections
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/heroSection.tsx
+++ src/components/feature/heroSection.tsx
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 export default function HeroSection(){
     return(
-        <div>ini hero</div>
+        <div className={"w-full flex justify-between px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
+            <div>text</div>
+            <div className={"rounded-xl"}>
+                <Image
+                    src={"/assets/img/banner.jpg"}
+                    alt={"banner"}
+                    width={720}
+                    height={720}
+                    className={"object- rounded-xl"}
+                />
+            </div>
+        </div>
     )
 }
