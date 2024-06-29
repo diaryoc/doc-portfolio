522529a49590c34f18587c8313d0548f23999ef6
diaryoc <diarycuriosity@gmail.com>
2024-06-29 16:38:26 +0700

    #3-add text hero sections
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/heroSection.tsx
+++ src/components/feature/heroSection.tsx
@@ -5,6 +5,9 @@ export default function HeroSection(){
         <div className={"w-full flex justify-between px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
             <div>
                 <h1>Diary of Curiosity</h1>
+                <p>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab autem consequatur, delectus dolore eligendi, eos explicabo minima, necessitatibus nisi officia quos repellat sint veritatis. Amet atque culpa ipsam nihil quasi!</span>
+                </p>
             </div>
             <div className={"rounded-xl"}>
                 <Image
