7d2f0571ab6be792170d492058a803f7c7245ee9
diaryoc <diarycuriosity@gmail.com>
2024-06-29 16:38:00 +0700

    #3-add text hero sections
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/heroSection.tsx
+++ src/components/feature/heroSection.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function HeroSection(){
     return(
         <div className={"w-full flex justify-between px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
-            <div>text</div>
+            <div>
+                <h1>Diary of Curiosity</h1>
+            </div>
             <div className={"rounded-xl"}>
                 <Image
                     src={"/assets/img/banner.jpg"}
