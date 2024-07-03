b811bb043c4fc6e7d7a03bfec55a171a9ae51bbf
diaryoc <diarycuriosity@gmail.com>
2024-07-04 06:41:41 +0700

    #7 - fix footer
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/footerSection.tsx
+++ src/components/feature/footerSection.tsx
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function FooterSection(){
     return(
-        <div>ini Footer</div>
+        <div className={"flex justify-between items-center h-20 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
+            <div className={"flex gap-4"}>
+                <Link href="/">
+                    <Image src={"/assets/img/icon/wa.png"} alt={"wa"} width={30} height={30} />
+                </Link>
+                <Link href="/">
+                    <Image src={"/assets/img/icon/instagram.png"} alt={"instagram"} width={30} height={30} />
+                </Link>
+                <Link href="/">
+                    <Image src={"/assets/img/icon/linkedin.png"} alt={"linkedin"} width={30} height={30} />
+                </Link>
+                <Link href="/">
+                    <Image src={"/assets/img/icon/github.png"} alt={"github"} width={30} height={30} />
+                </Link>
+            </div>
+            <div className={"font-bold text-[#2f2f2f] text-xl"}>Copyright©️2024</div>
+        </div>
     )
 }
