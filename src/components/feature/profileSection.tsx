578cc9c2c015d178b180936a0afd69f49f287476
diaryoc <diarycuriosity@gmail.com>
2024-07-06 16:41:32 +0700

    #4 - set up profile section
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/profileSection.tsx
+++ src/components/feature/profileSection.tsx
@@ -1,5 +1,8 @@
 export default function ProfileSection(){
     return(
-        <div>ini profile</div>
+        <div className={"flex justify-between mt-6 px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
+            <div className={"w-1/2"}>foto bulat</div>
+            <div className={"w-1/2 flex ml-8"}>deskripsi</div>
+        </div>
     )
 }
