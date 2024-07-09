481bbbca26db4d7e0225222e833a783a5e4ee556
diaryoc <diarycuriosity@gmail.com>
2024-07-09 20:39:05 +0700

    hot fix
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/sosmedSection.tsx
+++ src/components/feature/sosmedSection.tsx
@@ -1,5 +1,5 @@
 export default function SocialMediaSection(){
     return(
-        <div>ini sosmed</div>
+        <div className={"px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>ini sosmed</div>
     )
 }
