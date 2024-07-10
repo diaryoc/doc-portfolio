479b2f5113a3b2c4b662c3b2246551b224fe943f
diaryoc <diarycuriosity@gmail.com>
2024-07-10 15:43:17 +0700

    #8 - refactoring social media section card
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/component/socialMediaCard.tsx
+++ src/components/component/socialMediaCard.tsx
@@ -7,8 +7,8 @@ export default function SocialMediaCard(props:{
     imgBlack:string;
 }){
     return (
-        <div className={"w-full md:w-1/2 aspect-square"}>
-            <div className="group aspect-square m-3 flex flex-col items-center justify-center gap-2 overflow-hidden border-2 border-ternary hover:bg-primary hover:border-0">
+        <div className={"w-1/2 md:w-1/4 aspect-square"}>
+            <div className="group aspect-square m-3 flex flex-col items-center justify-center gap-2 overflow-hidden border-2 border-ternary hover:bg-primary hover:border-0 rounded-xl">
                 <div className={"flex items-center"}>
                     <Image src={props.imgBlack} alt={"icon black"} width={150} height={150} className={"group-hover:hidden"}/>
                     <Image src={props.imgWhite} alt={"icon white"} width={150} height={150} className={"hidden group-hover:flex"}/>
@@ -16,8 +16,8 @@ export default function SocialMediaCard(props:{
                 <div className={"flex items-center"}>
                     <h1 className={"font-semibold text-3xl text-primary group-hover:text-contras"}>{props.title}</h1>
                 </div>
-                <div className={"max-h-20 w-full px-2 flex text-center text-sm overflow-y-scroll text-secondary group-hover:text-contras"}>
-                    {props.description}
+                <div className={"max-h-20 w-full px-5 flex items-center text-sm text-secondary group-hover:text-contras"}>
+                        {props.description}
                 </div>
             </div>
         </div>
