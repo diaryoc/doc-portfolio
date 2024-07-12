eb55fe0525f5ad1abdf921f2b4ea64a8b3583b2f
diaryoc <diarycuriosity@gmail.com>
2024-07-12 08:26:54 +0700

    #8 - adding socmed list
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

---
+++ src/data/socmed.ts
@@ -0,0 +1,34 @@
+const socmed = [
+    {
+        id:1,
+        title:"github",
+        description:"github accounts",
+        imgBlack:"/assets/img/icon/github.png",
+        imgWhite:"/assets/img/icon/github.png"
+    },
+    {
+        id:2,
+        title:"instagram",
+        description:"instagram accounts",
+        imgBlack:"/assets/img/icon/instagram.png",
+        imgWhite:"/assets/img/icon/instagram.png"
+    },
+    {
+        id:3,
+        title:"Linked In",
+        description:"Linked In Accounts",
+        imgBlack:"/assets/img/icon/linkedin.png",
+        imgWhite:"/assets/img/icon/linkedin.png"
+    },
+    {
+        id:4,
+        title:"Whatsapp",
+        description:"Whatsapp Accounts",
+        imgBlack:"/assets/img/icon/wa.png",
+        imgWhite:"/assets/img/icon/wa.png"
+    },
+]
+
+export const getSocmed = ()=>{
+    return socmed;
+}
