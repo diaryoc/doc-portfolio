5915da1bf76278e3f5cf430ebbed2bed930c0cf6
diaryoc <diarycuriosity@gmail.com>
2024-07-12 08:31:51 +0700

    #8 - refactoring socmed list
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/sosmedSection.tsx
+++ src/components/feature/sosmedSection.tsx
@@ -1,12 +1,22 @@
 import SocialMediaCard from "@/components/component/socialMediaCard";
+import {getSocmed} from "@/data/socmed";
 
 export default function SocialMediaSection(){
+    const dataSocmed = getSocmed();
     return(
         <div className={"px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
             <h1 className={"text-5xl text-primary font-extrabold mb-3 mt-12 capitalize"}>Social Media</h1>
             <div className={"w-24 h-[4px] bg-primary mb-12"}></div>
             <div className="flex flex-wrap">
-                <SocialMediaCard title={"Instagram"} description={"Alvillage"} imgBlack={"/assets/img/icon/instagram.png"} imgWhite={"/assets/img/icon/instagram.png"}/>
+                {dataSocmed.map((item)=>(
+                    <SocialMediaCard
+                        title={item.title}
+                        description={item.description}
+                        imgBlack={item.imgBlack}
+                        imgWhite={item.imgWhite}
+                        key={item.id}
+                    />
+                ))}
             </div>
         </div>
     )
