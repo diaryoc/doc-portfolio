30a19f464e929469bc76aa6f5d87046c6f34a8de
diaryoc <diarycuriosity@gmail.com>
2024-07-13 17:54:08 +0700

    #6 - adding client section header
    Signed-off-by: diaryoc <diarycuriosity@gmail.com>

--- src/components/feature/clientSection.tsx
+++ src/components/feature/clientSection.tsx
@@ -1,5 +1,8 @@
 export default function ClientSection(){
     return(
-        <div className={"px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>ini client</div>
+        <div className={"px-4 md:px-6 lg:px-8 xl:px-8 2xl:px-32"}>
+            <h1 className={"text-5xl text-primary font-extrabold mb-3 mt-12 capitalize"}>Client</h1>
+            <div className={"w-24 h-[4px] bg-primary mb-12"}></div>
+        </div>
     )
 }
